14050f8b793dbda49faf10dab487973fd5bbe01a
Hahm, Seonyul <a71239511@gmail.com>
2025-03-26 02:16:04 +0900

    three

--- src/shared/Main.java
+++ src/shared/Main.java
@@ -3,5 +3,6 @@ package shared;
 public class Main {
 	public static void main(String[]args) {
 		System.out.println("one 202214015");
+		Three.print();
 	}
 }
---
+++ src/shared/Three.java
@@ -0,0 +1,7 @@
+package shared;
+
+public class Three {
+	public static void print() {
+		System.out.println("three");
+	}
+}
